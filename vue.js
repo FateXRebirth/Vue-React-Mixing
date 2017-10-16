e057011925f3ef549bc4ac53888ba7224ae2244f
FateXRebirth <FateXRebirth@gmail.com>
2017-10-16 11:08:04 +0800

    Vue exercise > The Vue Instance & Template Syntax

--- vue.js
+++ vue.js
@@ -1,8 +1,31 @@
-
 var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue.js!'
+    },
+    beforeCreate: function() {
+        console.log("beforeCreate");
+    },
+    created: function() {
+        console.log("created");
+    },
+    beforeMount: function() {
+        console.log("beforeMount");
+    },
+    mounted: function() {
+        console.log('mounted');
+    },
+    beforeUpdate: function() {
+        console.log('beforeUpdate');
+    },
+    updated: function() {
+        console.log('updated');
+    },
+    beforeDestroy: function() {
+        console.log('beforeDestroy');
+    },
+    destroyed: function() {
+        console.log('destroyed');
     }
 })
 
