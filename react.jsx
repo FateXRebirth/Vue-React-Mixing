a01a553d1692cc56bd1264b347a0f3dadf072a7a
FateXRebirth <FateXRebirth@gmail.com>
2017-11-01 12:00:33 +0800

    React exercise > Components and Props

--- react.jsx
+++ react.jsx
@@ -1,10 +1,62 @@
 function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-  }
+  return user.firstName + ' ' + user.lastName;
+}
   
 const user = {
   firstName: 'Harper',
   lastName: 'Perez'
+};
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+         src={props.user.avatarUrl}
+         alt={props.user.name} />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
 };
 
 function tick() {
@@ -14,6 +66,13 @@ function tick() {
       Hello, {formatName(user)}!
     </h1>
     <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    <Welcome name="Sara" />
+    <Welcome name="Cahal" />
+    <Welcome name="Edite" />
+    <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author} />
     </div>
   );
   ReactDOM.render(
