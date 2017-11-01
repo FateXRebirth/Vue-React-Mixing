8cb2f729ff46d4127addd1c8343491a827ff5de2
FateXRebirth <FateXRebirth@gmail.com>
2017-11-01 11:52:11 +0800

    React exercise > Rendering Elements

--- react.jsx
+++ react.jsx
@@ -2,18 +2,23 @@ function formatName(user) {
     return user.firstName + ' ' + user.lastName;
   }
   
-  const user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
-  };
-  
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+function tick() {
   const element = (
+    <div>
     <h1>
       Hello, {formatName(user)}!
     </h1>
+    <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
   );
-  
   ReactDOM.render(
     element,
     document.getElementById('root')
   );
+}
+setInterval(tick, 1000);
