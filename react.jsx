f46f2487a4a8ccd0326fa821d6545a7c33ec5d15
FateXRebirth <FateXRebirth@gmail.com>
2017-11-03 19:45:05 +0800

    React exercise > Handling Events

--- react.jsx
+++ react.jsx
@@ -92,6 +92,44 @@ class Clock extends React.Component {
   }
 }
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+class LoggingButton extends React.Component {
+  handleClick() {
+    console.log('this is:', this);
+  }
+
+  render() {
+    // This syntax ensures `this` is bound within handleClick
+    return (
+      <button onClick={(e) => this.handleClick(e)}>
+        Click me
+      </button>
+    );
+  }
+}
 
 const element = (
     <div>
@@ -110,6 +148,9 @@ const element = (
     <br/>
     <Clock />
     <br/>
+    <Toggle />
+    <br/>
+    <LoggingButton />
     </div>
 );
 
